9ee11dec930a4e2eba7709196df9d4dc0c314671
Tran Tien Thuong <thuongtt@edsolabs.com>
2021-10-12 01:32:28 +0700

    [TL5] drag and drop

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import "./Card.scss";
+import React from 'react';
+import './Card.scss';
 
 export default function Card(props) {
   const { card } = props;
   return (
-    <li className="card-item">
-      {card.cover && <img src={card.cover} alt="TEST"></img>}
+    <div className="card-item">
+      {card.cover && (
+        <img
+          src={card.cover}
+          alt="TEST"
+          onMouseDown={(e) => e.preventDefault()}
+        ></img>
+      )}
       {card.title}
-    </li>
+    </div>
   );
 }
